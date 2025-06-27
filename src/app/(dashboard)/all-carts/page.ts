172f12e7066b97b57fc6f8dbcb5e60e8fe8327d1
albertmanuel <albertmanuels10@gmail.com>
2025-06-28 02:44:08 +0700

    feat: add new route cart

--- src/app/(dashboard)/all-carts/page.ts
+++ src/app/(dashboard)/all-carts/page.ts
@@ -3,5 +3,5 @@ import { Metadata } from "next"
 export {default} from "@/components/views/AllCartsPage"
 
 export const metadata: Metadata = {
-  title: "Cart | Bosshire store"
+  title: "All Carts | Bosshire store"
 }
