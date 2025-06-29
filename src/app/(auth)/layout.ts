523d7c3b8f77ba649cf719cb3b504450933602cc
albertmanuel <albertmanuels10@gmail.com>
2025-06-29 15:56:12 +0700

    feat: update metadata

--- src/app/(auth)/layout.ts
+++ src/app/(auth)/layout.ts
@@ -1,7 +1,1 @@
-import { Metadata } from "next"
-
 export {default} from "@/components/layouts/AuthLayout"
-
-export const metadata: Metadata = {
-  title: "Login | Bosshire ecommerce"
-}
--- src/app/(dashboard)/page.ts
+++ src/app/(dashboard)/page.ts
@@ -1,1 +1,7 @@
+import { Metadata } from "next";
+
 export { default } from "@/components/views/DashboardPage";
+
+export const metadata: Metadata = {
+  title: "Dashboard | Bosshire store",
+}
