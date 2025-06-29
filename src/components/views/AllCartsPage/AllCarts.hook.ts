5841dbf259475dce1bd35ef2b4f18af3a59b96db
albertmanuel <albertmanuels10@gmail.com>
2025-06-29 14:21:13 +0700

    feat: refactor - all carts page

---
+++ src/components/views/AllCartsPage/AllCarts.hook.ts
@@ -0,0 +1,113 @@
+import { useEffect, useState } from "react";
+import { FormatListBulleted } from "@mui/icons-material";
+import useGetAllCarts from "@/services/useGetAllCarts";
+import { formatDateToLong } from "@/utils/date";
+import { useCartStore } from "@/stores/useCartStore";
+import useGetAllProducts from "@/services/useGetAllProducts";
+import { Product } from "@/types/product";
+import getEnrichedCart from "@/helpers/getEnrichedCarts";
+import { TableHeader } from "@/components/shared/Table/Table.types";
+
+const useAllCarts = () => {
+  const { allCarts, setToAllCarts } = useCartStore();
+
+  const [open, setOpen] = useState(false);
+  const [currentViewCartId, setCurrentViewCartId] = useState(null);
+
+  const { data: carts, isLoading, isSuccess } = useGetAllCarts();
+  const { data: products } = useGetAllProducts();
+
+  useEffect(() => {
+    if (allCarts.length == 0 && isSuccess) {
+      carts?.map((cart) => {
+        const enriched = getEnrichedCart(cart, products as Product[]);
+        setToAllCarts(enriched);
+      });
+    }
+  }, [allCarts.length, carts, isSuccess, products, setToAllCarts]);
+
+  const tableData = allCarts
+    ? allCarts.map((cart) => ({
+        ...cart,
+        id: cart.id,
+        userId: cart?.userId,
+        totalProducts: cart?.products.length,
+        totalQuantity: cart?.products.reduce(
+          (acc: number, curr: { quantity: number }) => acc + curr.quantity,
+          0
+        ),
+        createdAt: formatDateToLong(cart.date),
+      }))
+    : [];
+
+  const descOrderTableData = tableData.sort(
+    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+  );
+
+
+  const tableHeader: TableHeader[] = [
+    {
+      key: "index",
+      type: "index",
+      label: "No",
+      sx: {
+        width: "6%",
+      },
+    },
+    {
+      key: "id",
+      label: "Cart ID",
+      sx: {
+        width: "6%",
+      },
+    },
+    {
+      key: "userId",
+      label: "User ID",
+    },
+    {
+      key: "totalProducts",
+      label: "Total Products",
+    },
+    {
+      key: "totalQuantity",
+      label: "Total Quantity",
+    },
+    {
+      key: "createdAt",
+      label: "Created At",
+      sx: {
+        width: "10%",
+      },
+    },
+    {
+      key: "action",
+      type: "action",
+      label: "Actions",
+      actionOptions: () => [
+        {
+          icon: FormatListBulleted,
+          onClick: (row) => {
+            setOpen(true);
+            setCurrentViewCartId(row?.id);
+          },
+          color: "action",
+        },
+      ],
+      sx: {
+        width: "10%",
+      },
+    },
+  ];
+
+  return {
+    open,
+    setOpen,
+    currentViewCartId,
+    descOrderTableData,
+    isLoading,
+    tableHeader,
+  };
+};
+
+export default useAllCarts;
--- src/components/views/AllCartsPage/AllCartsPage.tsx
+++ src/components/views/AllCartsPage/AllCartsPage.tsx
@@ -1,109 +1,19 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Box, Typography } from "@mui/material";
 import Table from "@/components/shared/Table";
-import { TableHeader } from "@/components/shared/Table/Table";
-import { FormatListBulleted } from "@mui/icons-material";
-import useGetAllCarts from "@/services/useGetAllCarts";
-import { formatDateToLong } from "@/utils/date";
 import CartDetailModal from "./CartDetailModal";
-import { useCartStore } from "@/stores/useCartStore";
-import useGetAllProducts from "@/services/useGetAllProducts";
-import { Product } from "@/types/product";
-import getEnrichedCart from "@/helpers/getEnrichedCarts";
+import useAllCarts from "./AllCarts.hook";
 
 const AllCartsPage = () => {
-  const { allCarts, setToAllCarts } = useCartStore();
-
-  const [open, setOpen] = useState(false);
-  const [currentViewCartId, setCurrentViewCartId] = useState(null);
-
-  const { data: carts, isLoading, isSuccess } = useGetAllCarts();
-  const { data: products } = useGetAllProducts();
-
-  useEffect(() => {
-    if (allCarts.length == 0 && isSuccess) {
-      carts?.map((cart) => {
-        const enriched = getEnrichedCart(cart, products as Product[]);
-        setToAllCarts(enriched);
-      });
-    }
-  }, [allCarts.length, carts, isSuccess, products, setToAllCarts]);
-
-  const tableData = allCarts
-    ? allCarts.map((cart) => ({
-        ...cart,
-        id: cart.id,
-        userId: cart?.userId,
-        totalProducts: cart?.products.length,
-        totalQuantity: cart?.products.reduce(
-          (acc: number, curr: { quantity: number }) => acc + curr.quantity,
-          0
-        ),
-        createdAt: formatDateToLong(cart.date),
-      }))
-    : [];
-
-  const descOrderTableData = tableData.sort(
-    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
-  );
-
-  console.log("ALL CARTS: ", allCarts);
-
-  const tableHeader: TableHeader[] = [
-    {
-      key: "index",
-      type: "index",
-      label: "No",
-      sx: {
-        width: "6%",
-      },
-    },
-    {
-      key: "id",
-      label: "Cart ID",
-      sx: {
-        width: "6%",
-      },
-    },
-    {
-      key: "userId",
-      label: "User ID",
-    },
-    {
-      key: "totalProducts",
-      label: "Total Products",
-    },
-    {
-      key: "totalQuantity",
-      label: "Total Quantity",
-    },
-    {
-      key: "createdAt",
-      label: "Created At",
-      sx: {
-        width: "10%",
-      },
-    },
-    {
-      key: "action",
-      type: "action",
-      label: "Actions",
-      actionOptions: () => [
-        {
-          icon: FormatListBulleted,
-          onClick: (row) => {
-            setOpen(true);
-            setCurrentViewCartId(row?.id);
-          },
-          color: "action",
-        },
-      ],
-      sx: {
-        width: "10%",
-      },
-    },
-  ];
+  const {
+    open,
+    setOpen,
+    currentViewCartId,
+    descOrderTableData,
+    isLoading,
+    tableHeader,
+  } = useAllCarts();
 
   return (
     <Box sx={{ flexGrow: 1 }}>
